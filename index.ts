dc46aa89a34fea30005d37b0a5ab8efe4a4024a9
Lewis Moten <lewis.moten@deciphernow.com>
2021-02-02 15:18:28 -0500

    handle 404's

--- index.ts
+++ index.ts
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   console.log("got request");
   res.send("Root Folder");
 });
+app.use((req, res) => {
+  console.log("404", req.url);
+  res.status(404).json({ message: "four oh foured" });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening http://localhost:${PORT}`);
 });
