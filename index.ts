878fbe84adfe432e9ed7239f31c2ba9bd331740a
Lewis Moten <lewis.moten@deciphernow.com>
2021-02-02 16:52:33 -0500

    experiment with keyed collection and path parameters

--- index.ts
+++ index.ts
@@ -2,13 +2,40 @@ import express from "express";
 const app = express();
 const PORT = 8001;
 
+interface Document {
+  id: number;
+  name: string;
+}
+let doc: Document;
+const docs: { [key: number]: Document } = {
+  "23": {
+    id: 23,
+    name: "Twenty Three"
+  }
+};
 app.get("/", (req, res) => {
   console.log("got request");
   res.send("Root Folder");
 });
-app.get("/doc/:id", (req, res) => {
-  console.log("get doc by id: %s", req.params.id);
-  res.send(`where is doc ${req.params.id}?`);
+app.all("/doc/:id/:op?", function(req, res, next) {
+  const id = parseInt(req.params.id);
+  doc = docs[id];
+  if (doc) {
+    next();
+  } else {
+    next(new Error(`Cannot find doc ${id}`));
+  }
+});
+
+app.get("/doc/:id/edit", (req, res) => {
+  console.log("edit doc: %s", doc.name);
+  res.send(`I want to edit doc ${doc.name}?`);
+});
+app.get("/foo/:id/:bar?", (req, res) => {
+  const id = req.params.id;
+  const bar = req.params.bar;
+  console.log("get foo by id: %s %s", id, bar);
+  res.send(`where is foo ${id} ${bar}?`);
 });
 app.use((req, res) => {
   console.log("404", req.url);
