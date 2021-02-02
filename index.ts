c19cdf6d7c105454222f37a53a4c5f80dc09c94a
Lewis Moten <lewis.moten@deciphernow.com>
2021-02-02 16:01:41 -0500

    experiment with parameters

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   console.log("got request");
   res.send("Root Folder");
 });
+app.get("/doc/:id", (req, res) => {
+  console.log("get doc by id: %s", req.params.id);
+  res.send(`where is doc ${req.params.id}?`);
+});
 app.use((req, res) => {
   console.log("404", req.url);
   res.status(404).json({ message: "four oh foured" });
