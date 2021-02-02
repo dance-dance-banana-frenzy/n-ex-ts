0699b81a7baea9c7483ec50ea318418a295223e5
Lewis Moten <lewis.moten@deciphernow.com>
2021-02-02 13:53:45 -0500

    start express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 8001;
+
+function handleListen() {
+  console.log(`Listening http://localhost:${PORT}`);
+}
+
+function handleRootRequest(req, res) {
+  console.log("got request");
+  res.send("Root Folder");
+}
+
+app.get("/", handleRootRequest);
+app.listen(PORT, handleListen);
